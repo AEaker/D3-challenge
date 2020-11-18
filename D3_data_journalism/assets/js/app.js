0da8a70c052640742b3a61bc9101172d7a9d55b3
Aaron Eaker <aaroneaker@hotmail.com>
2020-11-17 20:16:19 -0500

    finished HW

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -15,7 +15,7 @@ var svg = d3
   .select("#scatter")
   .append("svg")
   .attr("width", svgWidth)
-  .attr("height", svgHeight + 40);
+  .attr("height", svgHeight);
 
 
 var chartGroup = svg.append("g")
@@ -39,44 +39,72 @@ d3.csv('./assets/data/data.csv').then(function(data) {
     });
 
     var xLinearScale = d3.scaleLinear()
-    .range([0, width]);
+    .range([0, width])
+    .domain(d3.extent(data, d => d.poverty));
 
     var yLinearScale = d3.scaleLinear()
-    .range([height, 0]);
+    .range([height, 0])
+    .domain(d3.extent(data, d => d.healthcare));
 
-    var bottomAxis = d3.axisBottom(xLinearScale);
-    var leftAxis = d3.axisLeft(yLinearScale);
+    var xAxis = d3.axisBottom(xLinearScale);
+    var yAxis = d3.axisLeft(yLinearScale);
 
-    var xAxis = chartGroup.append('g')
-    .classed('x-axis', true)
-    .attr('transform', `translate(0, ${height})`)
-    .call(bottomAxis);
+    chartGroup.append("g")
+    .classed("axis", true)
+    .call(yAxis);
 
-    var yAxis = chartGroup.append('g')
-      .classed('y-axis', true)
-      .call(leftAxis);
-
-      var circlesGroup = chartGroup.selectAll('circle')
-      .data(data)
-      .enter()
-      .append('circle')
-      .classed('stateCircle', true)
-      .attr('cx', d => xLinearScale(d[x]))
-      .attr('cy', d => yLinearScale(d[y]))
-      .attr('r', 14)
-      .attr('opacity', '.5');
+    chartGroup.append("g")
+    .classed("axis", true)
+    .attr("transform", "translate(0, " + height + ")")
+    .call(xAxis);
 
 
-      var textGroup = chartGroup.selectAll('.stateText')
-      .data(data)
-      .enter()
-      .append('text')
-      .classed('stateText', true)
-      .attr('x', d => xLinearScale(d[x]))
-      .attr('y', d => yLinearScale(d[y]))
-      .attr('dy', 3)
-      .attr('font-size', '10px')
-      .text(function(d){return d.abbr});
+
+  // append circles
+    var circlesGroup = chartGroup.selectAll("circle")
+    .data(data)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d.poverty))
+    .attr("cy", d => yLinearScale(d.healthcare))
+    .attr("r", "10")
+    .attr("fill", "steelblue")
+    .attr("stroke-width", "1")
+    .attr("stroke", "black");
+    //abbr
+    var textGroup = chartGroup.selectAll('.stateText')
+    .data(data)
+    .enter()
+    .append('text')
+    .classed('stateText', true)
+    .attr('x', d => xLinearScale(d.poverty))
+    .attr('y', d => yLinearScale(d.healthcare))
+    .attr('dy', 3)
+    .attr('font-size', '10px')
+    .text(function(d){return d.abbr});
+
+    //make labels for chart
+    var labelsGroup = chartGroup.append("g")
+    .attr("transform", `translate(${width / 2}, ${height + 20})`);
+
+      //x axis label
+    var PovertyLabel = labelsGroup.append("text")
+    .attr("class", "x label")
+    .attr("text-anchor", "end")
+    .attr("x", 0)
+    .attr("y", 40)
+    .text("In Poverty (in %)");
+    // y axis label
+    var HealthcareLabel = labelsGroup.append("text")
+    .attr("class", "y label")
+    .attr("text-anchor", "end")
+    .attr("x", 300)
+    .attr("y", -500)
+    .attr("dy", "2em")
+    .classed("axis=test", true)
+    .attr("transform", "rotate(-90)")
+    .text("Lacks Healthcare (in %)");
+
 
 
 }).catch(function(error) {
